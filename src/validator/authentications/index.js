6edb0d11337e9aecb34036dca3060bb36af81ad3
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-15 22:45:23 +0700

    joi aunthentications validator added

---
+++ src/validator/authentications/index.js
@@ -0,0 +1,29 @@
+const {
+  PostAuthenticationPayloadSchema,
+  PutAuthenticationPayloadSchema,
+  DeleteAuthenticationPayloadSchema,
+} = require('./schema');
+const InvarianError = require('../../exceptions/InvariantError');
+
+const AuthenticationsValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvarianError(validationResult.error.message);
+    }
+  },
+  validatePutAuthenticationPayload: (payload) => {
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvarianError(validationResult.error.message);
+    }
+  },
+  validateDeleteAuthenticationPayload: (payload) => {
+    const validationResult = DeleteAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvarianError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AuthenticationsValidator;
