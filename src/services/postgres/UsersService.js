ec202629497249f124108bfd91132f9a92f38aaf
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-08-15 22:44:25 +0700

    usersServices updated: add Verify user credential

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,7 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 const InvarianError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFounderror');
 
@@ -56,6 +57,27 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+    const match = await bcrypt.compare(password, hashedPassword);
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
   }
 }
 
